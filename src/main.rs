10143ce8bbcb8883c38d5fa752c5f991d04fbf68
Thomas Elder <tgelder@gmail.com>
2019-01-01 02:55:31 +0000

    Removing java comments

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,30 @@ pub mod utils;
 pub mod scale;
 pub mod mesh;
 
+use mesh::Mesh;
+
 fn main() {
+
+    // use rand::rngs::mock::StepRng;
+    // use std::u64;
+
+    // fn get_rng() -> Box<StepRng> {
+    //     Box::new(StepRng::new(u64::MAX / 2 + 1, 0))
+    // }
+
+    // let mut mesh = Mesh::new(2, 0.0);
+    // mesh.set_z_vector(vec![
+    //     vec![100.0, 200.0],
+    //     vec![300.0, 400.0]
+    // ]);
+
+    // let mut rng = get_rng();
+    // let range = (0.1, 0.9);
+
+    // for i in 0..14 {
+        
+    //     mesh = mesh.next(&mut rng, range);
+    //     println!("{}", mesh.get_width());
+    // }
    
 }
--- src/mesh.rs
+++ src/mesh.rs
@@ -83,6 +83,10 @@ impl Mesh {
         }
     }
 
+    pub fn get_width(&self) -> i32 {
+        self.width
+    }
+
     pub fn get_z_in_bounds(&self, x: i32, y: i32) -> f64 {
         self.z[x as usize][y as usize]
     }
@@ -150,7 +154,7 @@ impl Mesh {
 
         split_rules.sort_by(|a, b| a.range.0.partial_cmp(&b.range.0).unwrap());
 
-        SplitProcess{split_rules, splits: vec![]}
+        SplitProcess{split_rules, splits: Vec::with_capacity(4)}
     }
 
     fn split<R: Rng> (&self, x: i32, y: i32, rng: &mut Box<R>, random_range: (f64, f64)) -> Vec<Split> {
@@ -175,31 +179,7 @@ impl Mesh {
         }
         out
     }
-
-    
-
-  
-//   List<Split> splitCell(int x, int y, RNG rng, Scale scale) {
-
-//     List<SplitRule> splitRules = new ArrayList<>();
-
-//     for (int offsetX = 0; offsetX < 2; offsetX++) {
-//       for (int offsetY = 0; offsetY < 2; offsetY++) {
-//         int xNeighbour = (offsetX * 2) - 1;
-//         int yNeighbour = (offsetY * 2) - 1;
-//         double xNeighbourZ = getZ(x + xNeighbour, y, Mesh.MIN_VALUE);
-//         double yNeighbourZ = getZ(x, y + yNeighbour, Mesh.MIN_VALUE);
-//         double dNeighbourZ = getZ(x + xNeighbour, y + yNeighbour, Mesh.MIN_VALUE);
-//         double z = getZ(x, y);
-
-//         double minZ = Stream.of(xNeighbourZ, yNeighbourZ, dNeighbourZ, z).min(Double::compareTo).get();
-
-//         splitRules.add(new SplitRule( offsetX, offsetY, minZ, z));
-
-//       }
-//     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -207,7 +187,6 @@ mod tests {
     use std::u64;
     use super::*;
     use rand::rngs::mock::StepRng;
-    //use ::rand::StepRng;
 
     fn get_rng() -> Box<StepRng> {
         Box::new(StepRng::new(u64::MAX / 2 + 1, 0))
@@ -225,7 +204,7 @@ mod tests {
             offset_y: -1,
             z: 0.15537
         };
-        assert_eq!(rule.generate_split(&mut get_rng(), (0.1, 0.8)),expected);
+        assert_eq!(rule.generate_split(&mut get_rng(), (0.1, 0.8)), expected);
 
     }
 
@@ -316,174 +295,10 @@ mod tests {
         assert_eq!(actual, expected);
     }
 
+    #[test]
+    fn next_mesh_should_retain_downhill_property() {
+            
+    }
+
 
 }
-
-// class Mesh {
-
-//   static final double MAX_VALUE = Double.MAX_VALUE;
-//   static final double MIN_VALUE = Double.MIN_VALUE;
-    
-//   static final short [] dx8 = {-1, -1, 0, 1, 1, 1, 0, -1};
-//   static final short [] dy8 = {0, -1, -1, -1, 0, 1, 1, 1};
-
-//   //static final short [] dx4 = {-1, 0, 1, 0};
-//   //static final short [] dy4 = {0, -1, 0, 1};
-  
-//   @Getter
-//   private final int width;
-//   private double[][] z;
-
-//   Mesh(int width) {
-//     this.width = width;
-//     z = new double[width][width];
-//   }
-
-//   final double getZ(int x, int y) {
-//     return z[x][y];
-//   }
-  
-//   boolean inBounds(int x, int y){
-//     return x >= 0 && y >= 0 && x < width && y < width;
-//   }
-  
-//   double[][] getZ() {
-//     return z;
-//   }
-  
-//   double getZ(int x, int y, double outOfBoundsValue) {
-//     if (inBounds(x, y)) {
-//       return getZ(x, y);
-//     }
-//     else {
-//       return outOfBoundsValue;
-//     }
-//   }
-  
-//   final void setZ(int x, int y, double value) {
-//     z[x][y] = value;
-//   }
-  
-//   void setZ(double[][] values) {
-//     z = values;
-//   }
-  
-//   void setZ(double value) {
-//     iterate((x, y) -> setZ(x, y, value));
-//   }
- 
-//   double getMinZ() {
-//     double out = Mesh.MAX_VALUE;
-//     for (int x=0; x<width; x++) {
-//       for (int y=0; y<width; y++) {
-//         out = Math.min(out, getZ(x, y));
-//       }
-//     }
-//     return out;
-//   }
-  
-//   double getMaxZ() {
-//     double out = Mesh.MIN_VALUE;
-//     for (int x=0; x<width; x++) {
-//       for (int y=0; y<width; y++) {
-//         out = Math.max(out, getZ(x, y));
-//       }
-//     }
-//     return out;
-//   }
-  
-//   void iterate(MeshOperation operation) {
-//     for (int y = 0; y < width; y++) {
-//       for (int x = 0; x < width; x++) {
-//         operation.operate(x, y);
-//       }
-//     }
-//   }
-  
-//   <T extends Throwable> void iterateWithThrows(MeshOperationWithThrows<T> operation) throws T {
-//     for (int y = 0; y < width; y++) {
-//       for (int x = 0; x < width; x++) {
-//         operation.operate(x, y);
-//       }
-//     }
-//   }
-
-//   List<Split> splitCell(int x, int y, RNG rng, Scale scale) {
-
-//     List<SplitRule> splitRules = new ArrayList<>();
-
-//     for (int offsetX = 0; offsetX < 2; offsetX++) {
-//       for (int offsetY = 0; offsetY < 2; offsetY++) {
-//         int xNeighbour = (offsetX * 2) - 1;
-//         int yNeighbour = (offsetY * 2) - 1;
-//         double xNeighbourZ = getZ(x + xNeighbour, y, Mesh.MIN_VALUE);
-//         double yNeighbourZ = getZ(x, y + yNeighbour, Mesh.MIN_VALUE);
-//         double dNeighbourZ = getZ(x + xNeighbour, y + yNeighbour, Mesh.MIN_VALUE);
-//         double z = getZ(x, y);
-
-//         double minZ = Stream.of(xNeighbourZ, yNeighbourZ, dNeighbourZ, z).min(Double::compareTo).get();
-
-//         splitRules.add(new SplitRule(offsetX, offsetY, minZ, z));
-
-//       }
-//     }
-
-//     List<SplitRule> sorted = splitRules.stream()
-//         .sorted(Comparator.comparingDouble(sr -> sr.minZ))
-//         .collect(Collectors.toList());
-
-//     List<Split> out = new ArrayList<>();
-
-// //    System.out.println("Starting rules = " + sorted);
-
-//     for (SplitRule rule : sorted) {
-
-//       //System.out.println(rule.minZ);
-
-//       Split split = rule.generateSplit(rng, scale);
-
-
-//       out.add(split);
-
-//       for (SplitRule other : sorted) {
-//         if (other != rule) {
-//           if (other.offsetX == rule.offsetX || other.offsetY == rule.offsetY) {
-//             other.minZ = Math.min(other.minZ, split.z);
-
-//           }
-//         }
-//       }
-//     }
-
-// //    System.out.println("Final rules = " + sorted);
-// //    System.out.println("Final splits = " + out);
-
-//     return out;
-
-//   }
-
-//   @AllArgsConstructor
-//   @Data
-//   private static class SplitRule {
-//     final int offsetX;
-//     final int offsetY;
-//     double minZ;
-//     final double maxZ;
-
-//     private Split generateSplit(RNG rng, Scale scale) {
-//       double r = rng.getNext();
-//       double range = (maxZ - minZ);
-//       double z = minZ + range * scale.scale(r);
-
-//       return new Split(offsetX, offsetY, z);
-//     }
-//   }
-
-//   @AllArgsConstructor
-//   @Data
-//   static class Split {
-//     final int offsetX;
-//     final int offsetY;
-//     final double z;
-//   }
-  
