9734bbca0355499d14d7244010983c6f8e982886
Thomas Elder <tgelder@gmail.com>
2019-03-02 19:27:04 +0000

    Removed repetition from run()

--- src/main.rs
+++ src/main.rs
@@ -26,23 +26,24 @@ fn main() {
 
     let mut mesh = Mesh::new(1, 0.0);
     mesh.set_z(0, 0, MAX);
-    let seed = 2;
+    let seed = 3;
     let mut rng = Box::new(SmallRng::from_seed([seed; 16]));
 
     for i in 0..9 {
-        mesh = MeshSplitter::split(&mesh, &mut rng, (0.05, 0.5));
+        mesh = MeshSplitter::split(&mesh, &mut rng, (0.1, 0.9));
         if i < 9 {
-            let threshold = i * 2;
-            mesh = Erosion::erode(mesh, &mut rng, threshold, 8);
+            let threshold = i * i;
+            mesh = Erosion::erode(mesh, &mut rng, threshold, 16);
         }
         println!("{}-{}", i, mesh.get_width());
     }
     
-    mesh = mesh.rescale(&Scale::new((mesh.get_min_z(), mesh.get_max_z()), (0.0, 1.0)));
+    let max_z = 100.0;
+    mesh = mesh.rescale(&Scale::new((mesh.get_min_z(), mesh.get_max_z()), (0.0, max_z)));
     
     let terrain = mesh.get_z_vector().map(|z| z as f32);
 
-    let mut engine = IsometricEngine::new("Isometric", 1024, 1024, terrain);
+    let mut engine = IsometricEngine::new("Isometric", 1024, 1024, terrain, max_z as f32);
     
     engine.run();
    
